6fc74f851e939f323d280995ee4f79dedbc0929c
Rafael Villaça <rafaelvillaca1903@gmail.com>
2024-06-24 18:31:18 -0300

    Implementando lazy loading ao projeto, assim como a criação dos módulos login e register e seus respectivos componentes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full"
+  },
+  {
+    path: "login",
+    loadChildren: () => import("./modules/login/login.module").then(mod => mod.LoginModule)
+  },
+  {
+    path: "register",
+    loadChildren: () => import("./modules/register/register.module").then(mod => mod.RegisterModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
